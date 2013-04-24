304844ab33436271df7a1cef81423dcddaa3229b
Pravish <pravish.sood@nyu.edu>
2013-04-24 18:45:30 -0400

    The parser has where clause parsing implemented. This is mainly validation of SQL select statement. There are some known bugs however. Next task is to pull out the expressions from the search conditions

--- experiments/mongoODBC.cpp
+++ experiments/mongoODBC.cpp
@@ -1,17 +1,22 @@
 // mongoODBC.cpp : Defines the entry point for the console application.
 //
 
+#include "stdafx.h"
+#include <stdio.h>
 
 // #define BOOST_SPIRIT_DEBUG
 #include <boost/fusion/adapted.hpp>
 #include <boost/spirit/include/qi.hpp>
+#include <boost/variant.hpp>
 
 namespace qi = boost::spirit::qi;
 
+typedef boost::variant<double, std::string> double_n_string;	// string_literal, numeric_literal, boolean_literal;
+
 struct SelectStatement
 {
-    std::vector<std::string> columns, fromtables; 
-    std::string whereclause; // TODO model as a vector<WhereCondition> :) 
+    std::vector<std::string> columns, fromtables;
+    double_n_string whereclause; // TODO model as a vector<WhereCondition> :)
 
     friend std::ostream& operator<<(std::ostream& os, SelectStatement const& ss)
     {
@@ -23,26 +28,27 @@ struct SelectStatement
 		for(std::vector<int>::size_type i = 0; i != ss.columns.size(); i++) {
 			output.append(ss.columns[i]);
 			output.append(", ");
-			
+
 		}
 		//std::vector<int>::size_type i1=0;
 		output.append("\nTables: ");
 		for(std::vector<int>::size_type i = 0; i != ss.fromtables.size(); i++) {
 			output.append(ss.fromtables[i]);
 			output.append(", ");
-			
+
 		}
-		output.append("\nWHERE " + ss.whereclause);
+		//output.append("\nWHERE: " + ss.whereclause);
 		return os << output;
 		//return os << "SELECT " << std::endl << ss.columns.size() << " columns] from [" << ss.fromtables.size() << " tables]\nWHERE " + ss.whereclause;
     }
 };
 
-BOOST_FUSION_ADAPT_STRUCT(SelectStatement, 
+BOOST_FUSION_ADAPT_STRUCT(SelectStatement,
         (std::vector<std::string>, columns)
         (std::vector<std::string>, fromtables)
-        (std::string, whereclause)
+        (double_n_string, whereclause)
        )
+
 
 template <typename It, typename Skipper = qi::space_type>
     struct parser : qi::grammar<It, SelectStatement(), Skipper>
@@ -50,15 +56,56 @@ template <typename It, typename Skipper = qi::space_type>
     parser() : parser::base_type(start)
     {
         using namespace qi;
+		using boost::spirit::lit;
 
-        sqlident = lexeme [ alpha >> *alnum ]; // table or column name
-
-        columns  = no_case [ "select" ] >> (sqlident % ',');
+        sqlident = lexeme [ alpha >> *alnum ]; // table or column name	
+        columns  = no_case [ "select" ] >> -(no_case ["distinct"] | no_case ["ALL"]) >> (sqlident % ',');
         tables   = no_case [ "from" ]   >> (sqlident % ',');
+        //whr		 = no_case [ "where" ] >> (some_column);
+		
+		comparison_op = 
+		lit(">") | lit ("<") | lit ("<=") | lit (">=") | lit ("=") | lit ("<>");
+		
+		//boost::spirit::qi::string(">") | boost::spirit::qi::string("<") | boost::spirit::qi::string("<=");// | lit (">=") | lit ("=") | lit ("<>");
+
+		boolean_literal %= 
+			no_case ["TRUE"] | no_case ["FALSE"] | no_case ["YES"] | no_case ["NO"] | no_case ["TRUE"] | no_case ["ON"] | no_case ["OFF"];
+
+		numeric_literal %= 
+			double_ ;
+
+		string_literal %= 
+			lexeme['"' >> +(char_ - '"') >> '"'];
+
+		club_literal %= 
+			boolean_literal | numeric_literal | string_literal;
+
+		comparison_predicate %= 
+			column_identifier >> comparison_op >> club_literal;
+			//| club_literal >> comparison_op >> column_identifier;
+
+		column_identifier %= 
+			user_defined_name;
+
+		user_defined_name %= 
+			lexeme [boost::spirit::qi::alpha >> *boost::spirit::qi::alnum];
+
+		boolean_primary %= 
+			comparison_predicate 
+			| '(' >> search_condition >> ')';
+
+		boolean_factor %= 
+			-( no_case ["NOT"]) >> boolean_primary;
+
+		boolean_term %= 
+			boolean_factor >> -( no_case ["AND"] >> boolean_term);
+
+		search_condition %= 
+			boolean_term >> -(no_case ["OR"] >> search_condition);
 
         start    = columns >> tables 
             >> no_case [ "where" ]
-            >> lexeme [ +(char_ - ';') ]
+            >> search_condition//lexeme [ +(char_ - ';') ]
             >> ';';
 
         BOOST_SPIRIT_DEBUG_NODE(start);
@@ -69,8 +116,29 @@ template <typename It, typename Skipper = qi::space_type>
 
   private:
     qi::rule<It, std::string()             , Skipper> sqlident;
-    qi::rule<It, std::vector<std::string>(), Skipper> columns  , tables;
+    qi::rule<It, std::vector<std::string>(), Skipper> columns  , tables;//, whr;
     qi::rule<It, SelectStatement()         , Skipper> start;
+	qi::rule<It, std::string(), Skipper> compOp;
+	qi::rule<It, std::string(), Skipper> expression;
+	qi::rule<It, std::string(), Skipper>
+		boolean_literal,		//TRUE|FALSE|YES|NO|ON|OFF
+		string_literal,			// A sequence of one or more characters (any excluded characters? not for now)
+		comparison_op,			// < | > | <= | >= | = | <>
+		column_identifier,		// A string given by the user	
+		user_defined_name		// letter >> [letter | digit]
+		;
+	qi::rule <It, double_n_string(), Skipper> 
+		club_literal, 
+		comparison_predicate, 
+		boolean_primary, 
+		boolean_factor, 
+		boolean_term, 
+		search_condition
+		;
+	qi::rule<It, double(), Skipper > 
+		numeric_literal
+		;
+
 };
 
 template <typename C, typename Skipper>
@@ -84,7 +152,7 @@ template <typename C, typename Skipper>
     try
     {
         bool ok = qi::phrase_parse(f,l,p,skipper,query);
-        if (ok)   
+        if (ok)
         {
             std::cout << "parse success\n\n";
             std::cout << "query: " << query << "\n";
@@ -102,28 +170,25 @@ template <typename C, typename Skipper>
     return false;
 }
 
-//int main()
-//{
-//    const std::string input = "select id, name, price from books, authors where books.author_id = authors.id;";
-//    bool ok = doParse(input, qi::space);
-
-//    return ok? 0 : 255;
-//}
-
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-
-	std::cout << "mongoODBC connector" << std::endl << "sql >> ";
-
-	std::string sqlinput; // = "select id, name, price from books, authors where books.author_id = authors.id;";
-	std::getline(std::cin,sqlinput);
-    bool ok = doParse(sqlinput, qi::space);
+	std::cout << "mongoODBC connector" << std::endl << "sql [q/Q to quit] >> ";
 	
+	bool ok;
+	std::string sqlinput;
+	while (std::getline(std::cin,sqlinput))
+	{
+		if (sqlinput.empty() || sqlinput[0] == 'q' || sqlinput[0] == 'Q')
+			break;
 	
-	std::string exitstring;
-    std::cin >> exitstring;
+		// = "select id, name, price from books, authors where books.author_id = authors.id;";
+		
+		ok = doParse(sqlinput, qi::space);	
 
-    return ok? 0 : 255;
+		std::cout << std::endl << "sql [q/Q to quit] >> ";
+	}
+
+    return 0;
 }
 
