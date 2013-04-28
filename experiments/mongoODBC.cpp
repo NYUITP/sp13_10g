76e955d79fcb3d950e46acfde13119b4edf92e00
Pravish <pravish.sood@nyu.edu>
2013-04-28 12:26:55 -0400

    Where clause with a single preidcate is extracted fine. 2 or more predicates are append to just one string, rendering it useless

--- experiments/mongoODBC.cpp
+++ experiments/mongoODBC.cpp
@@ -16,7 +16,7 @@ typedef boost::variant<double, std::string> double_n_string;	// string_literal, 
 struct SelectStatement
 {
     std::vector<std::string> columns, fromtables;
-    double_n_string whereclause; // TODO model as a vector<WhereCondition> :)
+    std::vector<std::string> whereclause; // TODO model as a vector<WhereCondition> :)
 
     friend std::ostream& operator<<(std::ostream& os, SelectStatement const& ss)
     {
@@ -37,6 +37,12 @@ struct SelectStatement
 			output.append(", ");
 
 		}
+		output.append("\nSearchConditions: ");
+		for(std::vector<int>::size_type i = 0; i != ss.whereclause.size(); i++) {
+			output.append(ss.whereclause[i]);
+			output.append(", ");
+
+		}
 		//output.append("\nWHERE: " + ss.whereclause);
 		return os << output;
 		//return os << "SELECT " << std::endl << ss.columns.size() << " columns] from [" << ss.fromtables.size() << " tables]\nWHERE " + ss.whereclause;
@@ -46,64 +52,76 @@ struct SelectStatement
 BOOST_FUSION_ADAPT_STRUCT(SelectStatement,
         (std::vector<std::string>, columns)
         (std::vector<std::string>, fromtables)
-        (double_n_string, whereclause)
+        (std::vector<std::string>, whereclause)
        )
 
+const std::string lt("<"); const std::string gt(">");
+const std::string lte("<="); const std::string gte(">=");
+const std::string et("="); const std::string net(">=");
+const std::string like("like"); const std::string between("between");
 
 template <typename It, typename Skipper = qi::space_type>
     struct parser : qi::grammar<It, SelectStatement(), Skipper>
 {
     parser() : parser::base_type(start)
     {
-        using namespace qi;
-		using boost::spirit::lit;
+        //using namespace qi;
+		using qi::lexeme;
+		//using qi::double_;
+		using qi::char_;
+		using qi::alpha;
+		using qi::alnum;
+		using qi::string;
+		using qi::no_case;
 
-        sqlident = lexeme [ alpha >> *alnum ]; // table or column name	
+
+
+        sqlident = lexeme [ alpha >> *alnum ]; // table or column name
         columns  = no_case [ "select" ] >> -(no_case ["distinct"] | no_case ["ALL"]) >> (sqlident % ',');
         tables   = no_case [ "from" ]   >> (sqlident % ',');
         //whr		 = no_case [ "where" ] >> (some_column);
-		
-		comparison_op = 
-		lit(">") | lit ("<") | lit ("<=") | lit (">=") | lit ("=") | lit ("<>");
-		
+
+		comparison_op =
+		string(lt) | string(gt) | string(lte) | string(gte) | string(et) | string(net) | string(like) | string(between);
+
 		//boost::spirit::qi::string(">") | boost::spirit::qi::string("<") | boost::spirit::qi::string("<=");// | lit (">=") | lit ("=") | lit ("<>");
 
-		boolean_literal %= 
+		boolean_literal %=
 			no_case ["TRUE"] | no_case ["FALSE"] | no_case ["YES"] | no_case ["NO"] | no_case ["TRUE"] | no_case ["ON"] | no_case ["OFF"];
 
-		numeric_literal %= 
-			double_ ;
+		numeric_literal %=
+			+qi::digit;
 
-		string_literal %= 
+		string_literal %=
 			lexeme['"' >> +(char_ - '"') >> '"'];
 
-		club_literal %= 
+		club_literal %=
 			boolean_literal | numeric_literal | string_literal;
 
-		comparison_predicate %= 
+		comparison_predicate %=
 			column_identifier >> comparison_op >> club_literal;
 			//| club_literal >> comparison_op >> column_identifier;
 
-		column_identifier %= 
+		column_identifier %=
 			user_defined_name;
 
-		user_defined_name %= 
+		user_defined_name %=
 			lexeme [boost::spirit::qi::alpha >> *boost::spirit::qi::alnum];
 
-		boolean_primary %= 
-			comparison_predicate 
+		boolean_primary %=
+			comparison_predicate
 			| '(' >> search_condition >> ')';
 
-		boolean_factor %= 
+		boolean_factor %=
 			-( no_case ["NOT"]) >> boolean_primary;
 
-		boolean_term %= 
+		boolean_term %=
 			boolean_factor >> -( no_case ["AND"] >> boolean_term);
 
-		search_condition %= 
+		search_condition =
 			boolean_term >> -(no_case ["OR"] >> search_condition);
 
-        start    = columns >> tables 
+        start    = columns >> tables
             >> no_case [ "where" ]
             >> search_condition//lexeme [ +(char_ - ';') ]
             >> ';';
@@ -124,18 +142,19 @@ template <typename It, typename Skipper = qi::space_type>
 		boolean_literal,		//TRUE|FALSE|YES|NO|ON|OFF
 		string_literal,			// A sequence of one or more characters (any excluded characters? not for now)
 		comparison_op,			// < | > | <= | >= | = | <>
-		column_identifier,		// A string given by the user	
+		column_identifier,		// A string given by the user
 		user_defined_name		// letter >> [letter | digit]
 		;
-	qi::rule <It, double_n_string(), Skipper> 
-		club_literal, 
-		comparison_predicate, 
-		boolean_primary, 
-		boolean_factor, 
-		boolean_term, 
+	//qi::rule <It, double_n_string(), Skipper>
+	qi::rule <It, std::string(), Skipper>
+		club_literal,
+		comparison_predicate,
+		boolean_primary,
+		boolean_factor,
+		boolean_term,
 		search_condition
 		;
-	qi::rule<It, double(), Skipper > 
+	qi::rule<It, std::string(), Skipper >
 		numeric_literal
 		;
 
@@ -174,17 +193,17 @@ template <typename C, typename Skipper>
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::cout << "mongoODBC connector" << std::endl << "sql [q/Q to quit] >> ";
-	
+
 	bool ok;
 	std::string sqlinput;
 	while (std::getline(std::cin,sqlinput))
 	{
 		if (sqlinput.empty() || sqlinput[0] == 'q' || sqlinput[0] == 'Q')
 			break;
-	
+
 		// = "select id, name, price from books, authors where books.author_id = authors.id;";
-		
-		ok = doParse(sqlinput, qi::space);	
+
+		ok = doParse(sqlinput, qi::space);
 
 		std::cout << std::endl << "sql [q/Q to quit] >> ";
 	}
