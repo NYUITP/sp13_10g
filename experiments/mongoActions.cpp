3ea6ad677b78b852a6d8eca80ad184a6fc456aaa
dck273 <dck273@nyu.edu>
2013-05-12 14:51:10 -0400

    editing out duplications

--- experiments/mongoActions.cpp
+++ experiments/mongoActions.cpp
@@ -25,14 +25,72 @@
 
 using namespace mongo;
 
+//  Used to Create qucik tests of mongo actions later incorportated into driver
+//
 
 
-// Execute Select Query and Return DB pointer
-std::auto_ptr<mongo::DBClientCursor> mongoSelect(DBClientConnection& c, string db, string collection, BSONObj whereCond){
+// build note
+// g++ mongoActions.cpp -pthread -Isrc -Isrc/mongo -lmongoclient -lboost_thread-mt -lboost_system-mt -lboost_filesystem-mt  -o mongoActions
+
+
+void printQueryResults(auto_ptr<DBClientCursor> cursor){
+    std::cout<<"Enter BSON DUMPER"<<std::endl;
+    
+    while( cursor->more() ) {
+        std::cout<<cursor->next().toString()<<std::endl;
+    }
+}
+
+void printSet(set<string> stringset){
+    
+    set<string>::const_iterator it; // declare an read-only iterator
+    it = stringset.begin(); // assign it to the start of the vector
+    while (it != stringset.end()) // while it hasn't reach the end
+    {
+        cout << *it << " "; // print the value of the element it points to
+        it++; // and iterate to the next element
+    }
+    cout << endl;
+}
+
+void simCount(DBClientConnection& c, string dbcoll, BSONObj bobject){
         
+    cout<<"\n simCount \n"<< endl;
+    
+    // BSONObj cx = BSONObjBuilder().append(fromjson("{age:12}")).obj();
+    
+    mongo::BSONObj cx = fromjson("{age: {$gt : 12, $lt : 40}}");
+    mongo::BSONObj dx = fromjson("{age: {$gt : 12}, age:{$lt : 40}}");
+    
+    
+    mongo::Query query = QUERY("age"<< NE << 12);
+    auto_ptr<mongo::DBClientCursor> cursor = c.query(dbcoll,dx);
+    
+    set<string> fieldnames;
+    
+    while( cursor->more() ) {
+        BSONObj p = cursor->next();
+        cout<< p.nFields() <<endl;
+        cout << p.getField("name").toString() << endl;
+        cout << p.getFieldNames(fieldnames) << endl;
+        cout << fieldnames.size() << endl;
+    }
+}
+
+
+std::auto_ptr<mongo::DBClientCursor> mongoSelect(DBClientConnection& c, string db, string collection, string fields,  BSONObj whereCond){
+    
+    //    
+    cout << "Hello mongoSelect" << endl;
+    
     std::auto_ptr<mongo::DBClientCursor> cursor = c.query(db+"."+collection, whereCond);
     return cursor;
+    
 }
+
+
+
+
 
 //  finds the names of the document with the most fields in a collection 
 std::set<string> getCollFields(DBClientConnection& c, string db, string collection){
@@ -68,4 +126,37 @@ std::set<string> getCollFields(DBClientConnection& c, string db, string collecti
         }
     }
     return fieldnames;
+}
+
+
+void run() {
+    mongo::DBClientConnection c;
+    c.connect("localhost");
+    mongo::BSONObj cx = fromjson("{age: {$gt : 12, $lt : 40}}");
+    
+    string db = "Monsters";
+    string collection = "ghosts";
+    string dbcoll = db+"."+collection;
+    cout<<"db.collection = "<<dbcoll<<endl;
+    simCount(c, dbcoll, cx);
+   
+    printSet(getCollFields(c, db, collection));
+    
+    
+    
+    mongo::BSONObj dx = fromjson("{age: {$gt : 12}, age:{$lt : 40}}");
+    
+    printQueryResults(mongoSelect(c, db, collection, "name", dx));
+}
+
+
+int main() {
+    try {
+        run();
+        std::cout << "connected ok" << std::endl;
+    } catch( const mongo::DBException &e ) {
+        std::cout << "caught " << e.what() << std::endl;
+    }
+    return EXIT_SUCCESS;
+    
 }
